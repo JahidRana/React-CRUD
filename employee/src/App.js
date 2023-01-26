4143968bd023f7e97543bf8f381aaa2321ba4cba
Jahid Rana <64037870+JahidRana@users.noreply.github.com>
2023-01-26 11:11:39 +0600

    React-CRUD

---
+++ employee/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import EmpListing from "./EmpListing";
+import EmpCreate from "./EmpCreate";
+import EmpDetail from "./EmpDetail";
+import EmpEdit from "./EmpEdit";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React CRUD</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EmpListing />}></Route>
+          <Route path="/employee/create" element={<EmpCreate />}></Route>
+          <Route path="/employee/detail/:empid" element={<EmpDetail />}></Route>
+          <Route path="/employee/edit/:empid" element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
